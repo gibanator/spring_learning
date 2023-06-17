2b8034bacef52dfe5cd7a1dddcd624f6354d2b3c
Georgii Gibizov <georgy.gibizov@gmail.com>
2023-06-17 18:52:07 +0300

    bean lifecycle practice

--- src/main/java/org/ggibizov/springlearn/ClassicalMusic.java
+++ src/main/java/org/ggibizov/springlearn/ClassicalMusic.java
@@ -1,6 +1,13 @@
 package org.ggibizov.springlearn;
 
 public class ClassicalMusic implements Music {
+    private ClassicalMusic(){}
+    public void onInit(){
+        System.out.println("Initializing...");
+    }
+    public void onDestroy(){
+        System.out.println("Destruction...");
+    }
     @Override
     public String getSong() {
         return "Concerto in E minor";
--- src/main/java/org/ggibizov/springlearn/TestSpring.java
+++ src/main/java/org/ggibizov/springlearn/TestSpring.java
@@ -11,18 +11,22 @@ public class TestSpring {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
         // Music music = context.getBean("musicBean", Music.class);
+        ClassicalMusic classicalMusic = context.getBean("classicalMusic", ClassicalMusic.class);
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
-        boolean eq = musicPlayer == musicPlayer1;
-        System.out.println(eq);
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
-        musicPlayer.playMusicList();
-        System.out.println(musicPlayer1.getName());
-        System.out.println(musicPlayer1.getVolume());
-        musicPlayer1.playMusicList();
-
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+//        boolean eq = musicPlayer == musicPlayer1;
+//
+//        System.out.println(eq);
+//
+//        System.out.println(musicPlayer.getName());
+//        System.out.println(musicPlayer.getVolume());
+//        musicPlayer.playMusicList();
+//
+//        System.out.println(musicPlayer1.getName());
+//        System.out.println(musicPlayer1.getVolume());
+//        musicPlayer1.playMusicList();
+//
         context.close();
     }
 }
